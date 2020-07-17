2c5252123ac5daa1b93ff6efd0c40b42ba26606b
Jake Pressman <jpress7639@gmail.com>
2020-07-16 21:44:49 -0400

    evening work - edit/delete/detail

---
+++ client/src/screens/MainMenu/MainMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../components/shared/Layout/Layout'
+import DropDown from '../../components/shared/DropdownMenu/DropDown'
+
+const Home = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
