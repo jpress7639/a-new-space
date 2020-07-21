8b6f06af063cab2e2a455d3d32f0282004cbac52
Edwin Vera <edwinvera@Edwins-MacBook-Air.local>
2020-07-21 16:51:21 -0400

    radio search working

--- client/src/components/shared/SongPlaylistLayout/SongLayout.jsx
+++ client/src/components/shared/SongPlaylistLayout/SongLayout.jsx
@@ -1,27 +1,53 @@
-import React, { Component } from "react"
-import playlist from "../../../playlist/playlist.json"
-import "./SongLayout.css"
+import React, { Component } from "react";
+import playlist from "../../../playlist/playlist.json";
+import "./SongLayout.css";
 // import {Link} from 'react-router-dom'
 
 class SongLayout extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playlist: [],
+      queriedRadioSearch: [],
+    };
+  }
+
+  async componentDidMount() {
+    const totalPlaylist = playlist;
+    this.setState({
+      playlist: totalPlaylist,
+      queriedRadioSearch: totalPlaylist,
+    });
+  }
+
+  handleSearch = (event) => {
+    const queriedRadioSearch = playlist.filter((radio) =>
+      radio.genre.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    this.setState({ queriedRadioSearch });
+  };
 
   render() {
-    const genre = [...new Set(playlist.map(genre => genre.genre))]
-    console.log(genre)
+    const genre = [...new Set(this.state.queriedRadioSearch.map((genre) => genre.genre))];
+    console.log(genre);
     return (
       <>
         <div className="playlist-menu">
-        <h1 className='playlist-title'>SPACE TANK</h1>
-          <input className='playlist-search-bar' placeholder='Search'></input>
-              {genre.map(station => 
-              <div className='station-container'>
-                <h3>{station} Station</h3>
-              </div>
-              )}
+          <h1 className="playlist-title">SPACE TANK</h1>
+          <input
+            className="playlist-search-bar"
+            onChange={this.handleSearch}
+            placeholder="Search"
+          ></input>
+          {genre.map((station) => (
+            <div className="station-container">
+              <h3>{station} Station</h3>
+            </div>
+          ))}
         </div>
-        </>
-        )
-    }
+      </>
+    );
+  }
 }
 
-export default SongLayout
+export default SongLayout;
