f9d93cb280b742c017fa40038d9cf8fd5319e534
galeazzij <galeazzij@users.noreply.github.com>
2020-07-21 17:14:27 -0400

    add search capability so user can search for specific radio genre

--- client/src/components/shared/SongPlaylistLayout/SongLayout.jsx
+++ client/src/components/shared/SongPlaylistLayout/SongLayout.jsx
@@ -1,29 +1,55 @@
-import React, { Component } from "react"
-import playlist from "../../../playlist/playlist.json"
-import "./SongLayout.css"
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import playlist from "../../../playlist/playlist.json";
+import "./SongLayout.css";
+// import {Link} from 'react-router-dom'
 
 class SongLayout extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playlist: [],
+      queriedRadioSearch: [],
+    };
+  }
+  //update initial values with full playlist
+  async componentDidMount() {
+    const totalPlaylist = playlist;
+    this.setState({
+      playlist: totalPlaylist,
+      queriedRadioSearch: totalPlaylist,
+    });
+  }
+  //update the queriedRadioSearch with entered values of search bar
+  handleSearch = (event) => {
+    const queriedRadioSearch = playlist.filter((radio) =>
+      radio.genre.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    this.setState({ queriedRadioSearch });
+  };
 
   render() {
-    const { id } = this.props
-    const genre = [...new Set(playlist.map(genre => genre.genre))]
+    const genre = [
+      ...new Set(this.state.queriedRadioSearch.map((genre) => genre.genre)),
+    ];
+    console.log(genre);
     return (
       <>
         <div className="playlist-menu">
-        <h1 className='playlist-title'>SPACE TANK</h1>
-          <input className='playlist-search-bar' placeholder='Search'></input>
-              {genre.map((station, idx) => 
-              <div className='station-container' key={idx}>
-                  <Link to={`/users/${id}/${station}`}>
-                    <h3>{station} Station</h3>
-                  </Link>
-              </div>
-              )}
+          <h1 className="playlist-title">SPACE TANK</h1>
+          <input
+            className="playlist-search-bar"
+            onChange={this.handleSearch}
+            placeholder="Search"
+          ></input>
+          {genre.map((station) => (
+            <div className="station-container">
+              <h3>{station} Station</h3>
+            </div>
+          ))}
         </div>
-        </>
-        )
-    }
+      </>
+    );
+  }
 }
 
-export default SongLayout
+export default SongLayout;
