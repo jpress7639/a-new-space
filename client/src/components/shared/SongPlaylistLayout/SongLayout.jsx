8d691af9d465d4d018348e55ad9594024feef535
Jake Pressman <jpress7639@gmail.com>
2020-07-21 21:45:55 -0400

    fix

--- client/src/components/shared/SongPlaylistLayout/SongLayout.jsx
+++ client/src/components/shared/SongPlaylistLayout/SongLayout.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom'
 import playlist from "../../../playlist/playlist.json";
 import "./SongLayout.css";
 // import {Link} from 'react-router-dom'
