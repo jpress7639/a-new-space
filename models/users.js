02c93b77335201958787bed94ce5e4b13ad0c540
Jake Pressman <jpress7639@gmail.com>
2020-07-23 21:37:27 -0400

    bug part 3

--- models/users.js
+++ models/users.js
@@ -7,7 +7,7 @@ const User = new Schema(
         lastName: { type: String, required: true },
         email: { type: String, required: true }, 
         password: { type: String, required: true},
-        imgURL: { type: String, required: true }
+        imgURL: { type: String, required: false }
     }, 
     { timestamps: true }
 )
